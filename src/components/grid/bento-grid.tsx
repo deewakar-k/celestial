48d4218b3db3d4962656f7653bb140d53131d78b
deewakar <dakshftw01@gmail.com>
2025-02-28 00:41:03 +0530

    ui chore

--- src/components/grid/bento-grid.tsx
+++ src/components/grid/bento-grid.tsx
@@ -7,18 +7,18 @@ export default function Bento() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-2 p-0 max-w-5xl mx-auto">
       <div className="lg:col-span-4 flex flex-col gap-2">
-        <div className="rounded-xl overflow-hidden">
+        <div className="rounded-xl overflow-hidden transition-transform duration-500 ease-out hover:scale-[1.02]">
           <CardsChat />
         </div>
-        <div className="rounded-xl overflow-hidden">
+        <div className="rounded-xl overflow-hidden transition-transform duration-500 ease-out hover:scale-[1.02]">
           <CardsCreateAccount />
         </div>
       </div>
       <div className="lg:col-span-8 flex flex-col gap-2">
-        <div className="rounded-xl overflow-hidden">
+        <div className="rounded-xl overflow-hidden transition-transform duration-500 ease-out hover:scale-[1.02]">
           <CardsMetric />
         </div>
-        <div className="rounded-xl overflow-hidden">
+        <div className="rounded-xl overflow-hidden transition-transform duration-500 ease-out hover:scale-[1.02]">
           <CardsDataTable />
         </div>
       </div>
